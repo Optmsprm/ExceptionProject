ac2f77936b4ef5da0a26b37383a3629e37fa785b
Alex R <Optmsprm323@gmail.com>
2023-12-03 05:43:04 -0500

    Added comments

--- ExceptionProject/ExceptionProject.cpp
+++ ExceptionProject/ExceptionProject.cpp
@@ -13,6 +13,7 @@ char character(char, int);
 int main()
 {
     
+    //Tests the function for an exception with the character 'a' and an offset of 1
     try
     {
         cout << character('a', 1) << endl;
@@ -22,6 +23,7 @@ int main()
         cout << exception;
     }
 
+    //Tests the function for an exception with the character 'a' and an offset of -1
     try
     {
         cout << character('a', -1) << endl;
@@ -31,6 +33,7 @@ int main()
         cout << exception;
     }
 
+    //Tests the function for an exception with the character 'Z' and an offset of -1
     try
     {
         cout << character('Z', -1) << endl;
@@ -40,6 +43,7 @@ int main()
         cout << exception;
     }
 
+    //Tests the function for an exception with the character '?' and an offset of 5
     try
     {
         cout << character('?', 5) << endl;
@@ -56,16 +60,19 @@ char character(char start, int offset)
 
     if (!isalpha(start))
     {
+        //Checks whether the initial character is a letter of the alphabet
         string invalidCharacterException = "ERROR: Not a letter.\n";
         throw invalidCharacterException;
     }
     else if (!isalpha(start + offset))
     {
+        //Checks whether the character + offset is a letter of the alphabet
         string invalidRangeException = "ERROR: Outside of range.\n";
         throw invalidRangeException;
     }
     else
     {
+        //Returns the character + offset
         return start + offset;
     }
 
