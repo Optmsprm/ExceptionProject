675c9469c58f1acafc551de5111e47c16f814a97
Alex R <Optmsprm323@gmail.com>
2023-12-03 05:20:36 -0500

    Defined the character function and added the first try/catch block.

--- ExceptionProject/ExceptionProject.cpp
+++ ExceptionProject/ExceptionProject.cpp
@@ -3,13 +3,43 @@
 // 12/2/2023
 
 #include <iostream>
+#include <string>
+#include <cctype>
 
 using namespace std;
+
+char character(char, int);
 
 int main()
 {
     
-
+    try
+    {
+        cout << character('a', 1) << endl;
+    }
+    catch (string exception)
+    {
+        cout << exception << endl;
+    }
 
 }
 
+char character(char start, int offset)
+{
+
+    if (!isalpha(start))
+    {
+        string invalidCharacterException = "ERROR: Not a letter.\n";
+        throw invalidCharacterException;
+    }
+    else if (!isalpha(start + offset))
+    {
+        string invalidRangeException = "ERROR: Outside of range.\n";
+        throw invalidRangeException;
+    }
+    else
+    {
+        return start + offset;
+    }
+
+}
